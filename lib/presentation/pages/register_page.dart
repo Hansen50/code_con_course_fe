dcd74d5f786aab6814f949a08d22e9c617be2ea1
Hansen <hansenbilly5@gmail.com>
2025-05-30 15:40:37 +0700

    Create Registration Page

--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -1,13 +1,21 @@
 import 'dart:math';
 
+import 'package:code_con_course_fe/data/code_con_repository.dart';
+import 'package:code_con_course_fe/domain/duitku_entities/create_reservation_params.dart';
 import 'package:code_con_course_fe/domain/entities/payment_method.dart';
+import 'package:code_con_course_fe/domain/entities/reservation.dart';
+import 'package:code_con_course_fe/domain/entities/tresult.dart';
 import 'package:code_con_course_fe/presentation/constant.dart';
 import 'package:code_con_course_fe/presentation/extensions/build_context_extension.dart';
 import 'package:code_con_course_fe/presentation/providers/payment_methods_provider.dart';
+import 'package:code_con_course_fe/presentation/providers/router_provider.dart';
 import 'package:code_con_course_fe/presentation/widgets/code_con_app_bar.dart';
+import 'package:code_con_course_fe/presentation/widgets/error_dialog.dart';
+import 'package:code_con_course_fe/presentation/widgets/registration_status_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class RegisterPage extends ConsumerStatefulWidget {
   const RegisterPage({super.key});
@@ -43,7 +51,7 @@ class _RegisterPageState extends ConsumerState<RegisterPage> {
                     children: [
                       sizedBoxHeight60,
                       const Text(
-                        'Pleaase fill in the form below to register '
+                        'Please fill in the form below to register '
                         'for the event',
                         style: TextStyle(fontSize: 16),
                       ),
@@ -54,7 +62,7 @@ class _RegisterPageState extends ConsumerState<RegisterPage> {
                           border: OutlineInputBorder(),
                           hintText: 'Enter your email',
                           helperText: 'Your email is used for your unique '
-                              'identifier. The same email address cannot be used '
+                              'identifier. The same email address cannont be used '
                               'to register multiple times.',
                           helperMaxLines: 2,
                           helperStyle: TextStyle(fontSize: 11),
@@ -110,7 +118,7 @@ class _RegisterPageState extends ConsumerState<RegisterPage> {
                                     value: selectedPaymentMethod,
                                   )
                                 : Text(data.asFailure.message),
-                            error: (error, StackTrace) =>
+                            error: (error, stackTrace) =>
                                 Text(error.toString()),
                             loading: () => blueProgressIndicator,
                           ),
@@ -118,7 +126,95 @@ class _RegisterPageState extends ConsumerState<RegisterPage> {
                       isLoading
                           ? pinkProgressIndicator
                           : ElevatedButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                if (selectedPaymentMethod != null &&
+                                    emailController.text.trim().isNotEmpty &&
+                                    nameController.text.trim().isNotEmpty) {
+                                  setState(() {
+                                    isLoading = true;
+                                  });
+
+                                  final params = CreateReservationParams(
+                                      paymentMethod:
+                                          selectedPaymentMethod!.code,
+                                      orderId:
+                                          'RSV${DateTime.now().millisecondsSinceEpoch}',
+                                      customerName: nameController.text,
+                                      customerEmail: emailController.text,
+                                      returnUrl: '',
+                                      totalPayment: 75000);
+
+                                  CodeConRepository()
+                                      .createReservation(params: params)
+                                      .then(
+                                    (value) {
+                                      switch (value) {
+                                        case Success<Reservation>(:final data):
+                                          if (context.mounted) {
+                                            context.show(
+                                                RegistrationStatusDialog(
+                                                    reservation: data));
+                                          }
+                                          launchUrl(Uri.parse(data.paymentUrl),
+                                              mode: LaunchMode
+                                                  .externalApplication);
+                                        case Failure<Reservation>(
+                                            :final message
+                                          ):
+                                          {
+                                            if (message ==
+                                                'Email already registered') {
+                                              if (context.mounted) {
+                                                context.show(const ErrorDialog(
+                                                    message:
+                                                        'Email already registered'));
+                                              }
+                                            } else {
+                                              CodeConRepository()
+                                                  .checkReservation(
+                                                      emailController.text)
+                                                  .then(
+                                                (value) {
+                                                  switch (value) {
+                                                    case Success<Reservation>(
+                                                        :final data
+                                                      ):
+                                                      {
+                                                        if (context.mounted) {
+                                                          context.show(
+                                                              RegistrationStatusDialog(
+                                                                  reservation:
+                                                                      data));
+                                                        }
+
+                                                        launchUrl(
+                                                            Uri.parse(data
+                                                                .paymentUrl),
+                                                            mode: LaunchMode
+                                                                .externalApplication);
+                                                      }
+                                                    case Failure<Reservation>():
+                                                      {
+                                                        if (context.mounted) {
+                                                          context.show(
+                                                              ErrorDialog(
+                                                                  message:
+                                                                      message));
+                                                        }
+                                                      }
+                                                  }
+                                                },
+                                              );
+                                            }
+                                          }
+                                      }
+                                      setState(() {
+                                        isLoading = false;
+                                      });
+                                    },
+                                  );
+                                }
+                              },
                               style: const ButtonStyle(
                                   backgroundColor:
                                       WidgetStatePropertyAll(secondaryColor)),
@@ -134,7 +230,9 @@ class _RegisterPageState extends ConsumerState<RegisterPage> {
                         children: [
                           const Text('Please click '),
                           InkWell(
-                            onTap: () {},
+                            onTap: () {
+                              ref.watch(routerProvider).goNamed('check');
+                            },
                             mouseCursor: WidgetStateMouseCursor.clickable,
                             child: const Text(
                               'Here',
